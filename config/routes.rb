353939c53ee45a4aabfcb39955bc7f8d6f30074b
Geremia Taglialatela <tagliala.dev@gmail.com>
2015-08-02 14:52:16 +0200

    Test url_for for index

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  localized do
+    resources :articles, only: :index
+  end
 end
