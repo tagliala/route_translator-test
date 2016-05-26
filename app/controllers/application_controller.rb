6fa4bdee473de14c4f6ab15da8417870b174a9a2
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-05-27 00:20:33 +0200

    Add a custom method to generate localised urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :localized_url_for
+
+  protected
+
+  def localized_url_for
+    proc do |locale|
+      url_for(only_path: true, locale: locale.to_s)
+    end
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,16 @@ class ArticlesController < ApplicationController
   def set_article
     @article = Article.friendly.find(params[:id])
   end
+
+  def localized_url_for
+    case action_name
+    when 'show'
+      proc do |locale|
+        I18n.with_locale(locale) do
+          article_path(@article)
+        end
+      end
+    else super
+    end
+  end
 end
